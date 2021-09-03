e37c5df8110cc61969dcf00cbf325395c19dbf62
Arnab Dutta <arnabduttasocial@gmail.com>
2021-09-03 22:56:49 +0530

    Updates

---
+++ precache-manifest.16df2a46677b244220ca199ea591046a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179d7ff8ffa7fb0647cce86f4969bbfa",
+    "url": "/infinix-gallery/index.html"
+  },
+  {
+    "revision": "fc66403fa04f2ba50f24",
+    "url": "/infinix-gallery/static/css/main.f8ca13c6.chunk.css"
+  },
+  {
+    "revision": "4ba1657428c5d5b8cb20",
+    "url": "/infinix-gallery/static/js/2.804f8fd1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/infinix-gallery/static/js/2.804f8fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc66403fa04f2ba50f24",
+    "url": "/infinix-gallery/static/js/main.5e6319b9.chunk.js"
+  },
+  {
+    "revision": "0664426a722de946ea04",
+    "url": "/infinix-gallery/static/js/runtime-main.ff69ce5e.js"
+  }
+]);
